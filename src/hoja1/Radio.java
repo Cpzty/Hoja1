1496edab8c544d4b1232afb1e1e4c7d7f6adcced
Josrodjr <Josrodjr@gmail.com>
2017-01-17 07:35:17 -0600

    Texto de la clase Radio
    constructor, metodos

--- src/hoja1/Radio.java
+++ src/hoja1/Radio.java
@@ -1,5 +1,59 @@
-public class Radio implements Interfaz{
+public class Radio implements Interface{
+
+private boolean estado;
+private int frecuenciaFM;
+private double frecuenciaAM;
+private double boton1;
+private double boton2;
+private double boton2;
+private double boton3;
+private double boton4;
+private double boton5;
+private double boton6;
+private double boton7;
+private double boton8;
+private double boton9;
+private double boton10;
+private double boton11;
+private double boton12;
+//atributos de la clase Radio
+public Radio(boolean estado, int frecuenciaAM, double frecuenciaFM, double boton1, double boton2, double boton3, double boton4, double boton5, double boton6, double boton7, double boton8, double boton9, double boton10, double boton11, double boton12){
+  this.estado = estado;
+  this.frecuenciaFM = frecuenciaFM;
+  this.frecuenciaAM = frecuenciaAM;
+  this.boton1 = boton1;
+  this.boton2 = boton2;
+  this.boton3 = boton3;
+  this.boton4 = boton4;
+  this.boton5 = boton5;
+  this.boton6 = boton6;
+  this.boton7 = boton7;
+  this.boton8 = boton8;
+  this.boton9 = boton9;
+  this.boton10 = boton10;
+  this.boton11 = boton11;
+  this.boton12 = boton12;
+}
+//constructor de la clase
+
+public void setEstado(boolean newestado){
+  this.estado = newestado;
+}
+
+public void setFrecuenciaFM(int newfrecuencia){
+  this.frecuenciaFM = newfrecuencia;
+}
+
+public void setFrecuenciaAM (double newfrecuencia){
+  this.frecuenciaAM = newfrecuencia;
+}
 
 
+
+
+
+
+@Override
+public void
 
 }
