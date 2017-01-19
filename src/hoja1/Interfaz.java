f8b7b4e60c48ae8d9129620567dbb05332eb23e4
rec16005 <rec16005@uvg.edu.gt>
2017-01-19 16:26:42 -0600

    Update Interfaz.java
    Comentado

--- src/hoja1/Interfaz.java
+++ src/hoja1/Interfaz.java
@@ -1,22 +1,47 @@
 public abstract class Interfaz{
 
+  
+  /**
+	 * Atributos
+	 */
   private void estado();
   private void frecuencia();
   private void cambiar(boolean a);
   private void guardar(int a);
   private float seleccionar (int a);
-//atributos
 
+  /**
+	 * Metodos abstractos
+	 */
+
+  /**
+	 * Se encarga del estado
+	 */
 abstract public void estdo();
-
+  
+  /**
+	 * Se encarga de la frecuencia
+	 */
 abstract public void frecuencia();
 
+  /**
+	 * Cambia emisora
+   *@param: boolean a
+	 */
 abstract public void cambiar(boolean a);
-
+  
+  /**
+	 * Guarda emisora
+   *@param: int a
+	 */
 abstract public void guardar(int a);
 
+  /**
+	 * Selecciona una de las emisoras guardadas
+   *@param: int a
+	 */
 abstract public void seleccionar(int a);
 
-//metodos abstractos de la clase
+
 
 }
